6310f50c576cf653e9c01222eaf19b2e092cef9e
Tim Pietrusky <timpietrusky@gmail.com>
2022-12-10 22:56:02 +0100

    refactor: don't use trunk texture

--- src/atoms/Fir1.js
+++ src/atoms/Fir1.js
@@ -4,22 +4,27 @@ export class Fir1 {
   constructor() {}
 
   async generate({ scene, gltf, textureLoader, envMap }) {
-    const texture_trunk = await textureLoader.load(
-      "textures/Fir1/SnowTrunk/Trunk_snow_DIFFUSE.png"
-    );
-    texture_trunk.wrapS = texture_trunk.wrapT = THREE.RepeatWrapping;
-    const material_trunk = new THREE.MeshStandardMaterial({
-      map: texture_trunk,
-      envMap,
-    });
+    // const texture_trunk = await textureLoader.load(
+    //   "textures/Fir1/SnowTrunk/Trunk_snow_DIFFUSE.png"
+    // );
+    // texture_trunk.encoding = THREE.sRGBEncoding;
+    // texture_trunk.wrapS = texture_trunk.wrapT = THREE.RepeatWrapping;
+
+    // const material_trunk = new THREE.MeshStandardMaterial({
+    //   map: texture_trunk,
+    //   envMap,
+    // });
 
     const texture_bark = await textureLoader.load(
       "textures/Fir1/bark_DIFFUSE.png"
     );
+    texture_bark.encoding = THREE.sRGBEncoding;
     texture_bark.wrapS = texture_bark.wrapT = THREE.RepeatWrapping;
+
     const texture_bark_normal = await textureLoader.load(
       "textures/Fir1/bark_NORMAL.png"
     );
+    texture_bark_normal.encoding = THREE.sRGBEncoding;
     texture_bark_normal.wrapS = texture_bark_normal.wrapT =
       THREE.RepeatWrapping;
 
@@ -32,12 +37,17 @@ export class Fir1 {
     const texture_needless = await textureLoader.load(
       "textures/needles_snow/Needles1K/NeedlessSnow.png"
     );
+    texture_needless.encoding = THREE.sRGBEncoding;
+
     const texture_needless_alpha = await textureLoader.load(
       "textures/needles_snow/Needles1K/Needless_Opacity.png"
     );
+    texture_needless_alpha.encoding = THREE.sRGBEncoding;
+
     const texture_needless_normal = await textureLoader.load(
       "textures/needles_snow/Needles1K/Needless_NORMAL.jpg"
     );
+
     const material_needless = new THREE.MeshStandardMaterial({
       map: texture_needless,
       alphaMap: texture_needless_alpha,
@@ -52,14 +62,14 @@ export class Fir1 {
     gltf.scene.traverse((child) => {
       if (child.type === "Mesh") {
         if (child.material.name === "Trunk_fir_1") {
-          child.material = material_trunk;
-        }
-
-        if (child.material.name === "Bark_fir_2.001") {
           child.material = material_bark;
         }
 
-        if (child.material.name.includes("Needless")) {
+        if (child.material?.name === "Bark_fir_2.001") {
+          child.material = material_bark;
+        }
+
+        if (child.material?.name.includes("Needless")) {
           child.material = material_needless;
         }
       }
